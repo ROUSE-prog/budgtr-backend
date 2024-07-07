421096d4bcb9d2aa12978137fa972e1aa7002496
Steven Rouse <52251052+ROUSE-prog@users.noreply.github.com>
2024-07-07 15:36:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
-const app = express();
+const app = require("./app")
 
-mongoose.connect('mongodb://localhost/budgtr', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+require("dotenv").config()
+const PORT = process.env.PORT
 
-app.use(cors());
-app.use(bodyParser.json());
 
-const transactionsRouter = require('./routes/transactions'); // Ensure this path is correct
-app.use('/api', transactionsRouter);
-
-const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Listening on port ${PORT}`)
+})
