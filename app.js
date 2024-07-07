9fee084e81b1c27f982f0b37dbdf147a741d5870
Steven Rouse <52251052+ROUSE-prog@users.noreply.github.com>
2024-07-07 15:37:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-
 const express = require("express")
 const cors = require("cors")
 
-
 const app = express()
-
 
 app.use(express.json())
 app.use(cors())
 
-
 const transactionsRoutes = require("./routes/transactions")
-
 app.use("/transactions", transactionsRoutes)
-
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to My Bank Transaction App")
 })
 
-
 app.get("*", (req, res) => {
     res.status(404).json({ error: "Sorry, no page found" });
 });
-
 
 module.exports = app
